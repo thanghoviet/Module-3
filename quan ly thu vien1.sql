85e5df24ae35e09f801c97a846a6ed653e18b5f3
thang.hoviet <hovietthang1998@gmail.com>
2021-01-15 09:32:16 +0700

    first commit

---
+++ quan ly thu vien1.sql
@@ -0,0 +1,8 @@
+SELECT * FROM quanlythuvien.sinhvien;
+use qanlythuvien;
+insert into sinhvien (MaHocSinh,TenSinhVien,NgaySinh,DiaChi,Email,SDT,Anh,SoSachDangMuon)
+values
+(1,'Hoài Sơn',2000-01-08,'45 trần phú','hue@gmail.com',122942211,'q',0),
+(2,'Viết Thắng',1998-02-02,'131 Trần phú','danang2gmail.com',231653232,'q',0),
+(3,'Thân Văn phúc',1999-08-08,'44 AN Bằng','saigon@gmail.com',123465489,'q',3),
+(4,'Nhật thy',1998-06-05,'99 An dương','Nghean@gmail.com',554134155,'q',0);
